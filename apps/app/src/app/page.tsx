78735d86149baecf16fc9ef9cb729cae09dd67ac
Vinit <vinitnagar56@gmail.com>
2025-09-05 21:50:34 +0530

    analytics intialized

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,9 +1,10 @@
 import StackGenerator from "@/components/Generator";
-
+import { Analytics } from "@vercel/analytics/next"
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50">
       <StackGenerator />
+      <Analytics />
     </main>
   );
 }
